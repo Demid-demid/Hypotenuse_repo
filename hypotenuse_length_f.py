2745900d3f324ac3b75451346f57fdde27b83e2d
Demid <demidmarbella@gmail.com>
2020-06-18 11:41:43 +0200

    Name file changed

---
+++ hypotenuse_length_f.py
@@ -0,0 +1,16 @@
+#This is the function that will help you find a hypotenuse from the Right triangele
+import math as m
+
+def hypotenuse(a_side,b_side):
+    c_squared = a_side**2 + b_side**2
+    square_root = m.sqrt(c_squared)
+    rounded = round(square_root,1)
+    print(rounded)
+
+lenght_of_first_side = float(input("Input one side of  triangle: "))
+lenght_of_second_side  = float(input("Input second side of  triangle: "))
+
+hypotenuse(lenght_of_first_side,lenght_of_second_side)
+
+
+
